85948df83eabb557c42d94620167b6d9c22ea57c
ietzel <67884277+ietzel@users.noreply.github.com>
2025-11-28 12:48:35 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,7 +1,12 @@
 public class Character {
-  Character(name, species, alignment, str, dex, con, int, wis, cha) {
+  Character(name, species, level, alignment, str, dex, con, int, wis, cha) {
     this.name = name;
     this.species = species;
+    this.level = level;
+    this.fort = Math.floor(level/3);
+    this.ref = Math.floor(level/3);
+    this.will = Math.floor(level/2)+2;
+    this.bab = Math.floor(level/2);
     this.alignment = alignment;
     this.str = str + this.species.str;
     this.dex = dex + this.species.dex;
@@ -10,4 +15,11 @@ public class Character {
     this.wis = wis + this.species.wis;
     this.cha = cha + this.species.cha;
   }
+  public leveling(amount) {
+    this.level += amount;
+    this.fort = Math.floor(level/3);
+    this.ref = Math.floor(level/3);
+    this.will = Math.floor(level/2)+2;
+    this.bab = Math.floor(level/2);
+  } 
 }
