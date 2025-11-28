e529a5d4175e23570fee7f600cf823acd3ac7582
ietzel <67884277+ietzel@users.noreply.github.com>
2025-11-28 12:30:15 -0500

    Create Species.java

---
+++ Species.java
@@ -0,0 +1,15 @@
+public class Species {
+  Species(name, str, dex, con, int, wis, cha, size, water, land, air) {
+    this.name = name;
+    this.str = str;
+    this.dex = dex;
+    this.con = con;
+    this.int = int;
+    this.wis = wis;
+    this.cha = cha;
+    this.size = size;
+    this.water = water;
+    this.land = land;
+    this.air = air;
+  }
+}
