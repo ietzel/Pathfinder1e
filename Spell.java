960a0dde014cb14bfefb2d2b03f103119c5fa815
ietzel <67884277+ietzel@users.noreply.github.com>
2025-11-28 12:54:53 -0500

    Create Spell.java

---
+++ Spell.java
@@ -0,0 +1,12 @@
+public class Spell {
+  public Spell(time, v, s, rangebase, rangelevel, duration, savingthrow, spellresistance) {
+    this.time = time;
+    this.v = v;
+    this.s = s;
+    this.rangebase = rangebase;
+    this.rangelevel = rangelevel;
+    this.duration = duration;
+    this.savingthrow = savingthrow;
+    this.spellresistance;
+  }
+}
