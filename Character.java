e0a19605731935c4e2e6f82ebc623475c02ebd6b
ietzel <67884277+ietzel@users.noreply.github.com>
2025-11-28 12:24:15 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,7 +1,13 @@
 public class Character {
-  Character(name, species, alignment, str, dex, con, int, wis, cha, size, water, land, air) {
+  Character(name, species, alignment, str, dex, con, int, wis, cha) {
     this.name = name;
     this.species = species;
-    alignmentstr, dex, con, int, wis, cha, size, waterlandair
+    this.alignment = alignment;
+    this.str = str;
+    this.dex = dex;
+    this.con = con;
+    this.int = int;
+    this.wis = wis;
+    this.cha = cha;
   }
 }
