fde8c57b77a580d232824bfbd2d892b39ae7519b
ietzel <67884277+ietzel@users.noreply.github.com>
2025-11-28 12:40:21 -0500

    Update Species.java

--- Species.java
+++ Species.java
@@ -8,6 +8,12 @@ public class Species {
     this.wis = wis;
     this.cha = cha;
     this.size = size;
+    if(this.size == "s" | "S") {
+      this.str -= 2;
+      this.dex += 2;
+    } else if(this.size == "l" | "L") {
+      this.str += 2;
+      this.dex -= 2;
     this.water = water;
     this.land = land;
     this.air = air;
