34d565c4dd6d6110467650398324d9c2064b2f09
ietzel <67884277+ietzel@users.noreply.github.com>
2025-11-29 17:57:33 -0500

    Update Spell constructor parameters
    Removed spellresistance parameter from constructor.

--- Spell.java
+++ Spell.java
@@ -1,12 +1,12 @@
 public class Spell {
-  public Spell(time, v, s, rangebase, rangelevel, duration, savingthrow, spellresistance) {
+  public Spell(time, v, s, cost, rangebase, rangelevel, duration, savingthrow) {
     this.time = time;
     this.v = v;
     this.s = s;
+    this.cost = cost;
     this.rangebase = rangebase;
     this.rangelevel = rangelevel;
     this.duration = duration;
     this.savingthrow = savingthrow;
-    this.spellresistance;
   }
 }
