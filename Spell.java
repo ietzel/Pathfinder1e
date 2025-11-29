2030009b745ce831f36d3391481981e81080d982
ietzel <67884277+ietzel@users.noreply.github.com>
2025-11-29 17:58:44 -0500

    Update Spell.java

--- Spell.java
+++ Spell.java
@@ -1,12 +1,13 @@
 public class Spell {
-  public Spell(time, v, s, cost, rangebase, rangelevel, duration, savingthrow) {
+  public Spell(time, v, s, cost, rangebase, rangelevel, durationbase, durationlevel, savingthrow) {
     this.time = time;
     this.v = v;
     this.s = s;
     this.cost = cost;
     this.rangebase = rangebase;
     this.rangelevel = rangelevel;
-    this.duration = duration;
+    this.durationbase = durationbase;
+    this.durationlevel = durationlevel;
     this.savingthrow = savingthrow;
   }
 }
